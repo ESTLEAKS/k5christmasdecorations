0561aeda9300224252ed06135d4bcd5bee7581ea
ESTLEAKS <leaksmidnightmafia@gmail.com>
2025-10-25 02:02:22 -0400

    Add Christmas display configuration
    Added Christmas decorations and props configuration for various locations around the map.

---
+++ config.lua
@@ -0,0 +1,115 @@
+Config = {}
+
+-- Legion Square MASSIVE Christmas Display
+Config.LegionSquare = {
+    -- Giant Christmas Tree (center)
+    {prop = 'prop_xmas_tree_int', coords = vector3(195.84, -933.98, 29.69), heading = 0.0, scale = 3.0},
+    
+    -- Santa Sleigh & Reindeer
+    {prop = 'prop_crashed_heli', coords = vector3(200.0, -933.0, 35.0), heading = 90.0, scale = 1.0}, -- Sleigh placeholder
+    
+    -- Large Gift Boxes around tree
+    {prop = 'prop_boxpile_07d', coords = vector3(198.0, -933.0, 30.0), heading = 45.0, scale = 1.5},
+    {prop = 'prop_boxpile_07d', coords = vector3(193.0, -933.0, 30.0), heading = 135.0, scale = 1.5},
+    {prop = 'prop_boxpile_07d', coords = vector3(195.0, -936.0, 30.0), heading = 225.0, scale = 1.5},
+    {prop = 'prop_boxpile_07d', coords = vector3(195.0, -930.0, 30.0), heading = 315.0, scale = 1.5},
+    
+    -- Snowmen
+    {prop = 'prop_xmas_ext', coords = vector3(190.0, -933.0, 30.0), heading = 90.0, scale = 1.0},
+    {prop = 'prop_xmas_ext', coords = vector3(201.0, -933.0, 30.0), heading = 270.0, scale = 1.0},
+    
+    -- Christmas Lights Posts
+    {prop = 'prop_streetlight_01', coords = vector3(192.0, -928.0, 30.0), heading = 0.0, scale = 1.0},
+    {prop = 'prop_streetlight_01', coords = vector3(199.0, -928.0, 30.0), heading = 0.0, scale = 1.0},
+    {prop = 'prop_streetlight_01', coords = vector3(192.0, -938.0, 30.0), heading = 0.0, scale = 1.0},
+    {prop = 'prop_streetlight_01', coords = vector3(199.0, -938.0, 30.0), heading = 0.0, scale = 1.0},
+    
+    -- Candy Canes
+    {prop = 'prop_beach_parasol_03', coords = vector3(188.0, -931.0, 30.0), heading = 0.0, scale = 1.0},
+    {prop = 'prop_beach_parasol_03', coords = vector3(203.0, -931.0, 30.0), heading = 0.0, scale = 1.0},
+    {prop = 'prop_beach_parasol_03', coords = vector3(188.0, -935.0, 30.0), heading = 0.0, scale = 1.0},
+    {prop = 'prop_beach_parasol_03', coords = vector3(203.0, -935.0, 30.0), heading = 0.0, scale = 1.0},
+}
+
+-- Christmas Props Around The Map
+Config.ChristmasProps = {
+    -- Pillbox Hospital
+    {prop = 'prop_xmas_tree_int', coords = vector3(298.18, -584.63, 43.26), heading = 0.0, scale = 2.0},
+    {prop = 'prop_boxpile_07d', coords = vector3(296.0, -584.0, 43.26), heading = 45.0, scale = 1.0},
+    {prop = 'prop_boxpile_07d', coords = vector3(300.0, -584.0, 43.26), heading = 315.0, scale = 1.0},
+    
+    -- LSPD Mission Row
+    {prop = 'prop_xmas_tree_int', coords = vector3(441.27, -981.93, 30.69), heading = 0.0, scale = 1.5},
+    {prop = 'prop_xmas_ext', coords = vector3(438.0, -982.0, 30.69), heading = 90.0, scale = 1.0},
+    {prop = 'prop_xmas_ext', coords = vector3(444.0, -982.0, 30.69), heading = 270.0, scale = 1.0},
+    
+    -- Parking Lot near LSPD
+    {prop = 'prop_boxpile_07d', coords = vector3(420.0, -985.0, 29.5), heading = 0.0, scale = 1.0},
+    {prop = 'prop_boxpile_07d', coords = vector3(425.0, -985.0, 29.5), heading = 90.0, scale = 1.0},
+    
+    -- Vinewood Sign Area
+    {prop = 'prop_xmas_tree_int', coords = vector3(686.0, 583.0, 130.0), heading = 0.0, scale = 3.0},
+    
+    -- Sandy Shores
+    {prop = 'prop_xmas_tree_int', coords = vector3(1961.21, 3748.84, 32.34), heading = 0.0, scale = 2.0},
+    {prop = 'prop_xmas_ext', coords = vector3(1958.0, 3749.0, 32.34), heading = 45.0, scale = 1.0},
+    {prop = 'prop_xmas_ext', coords = vector3(1964.0, 3749.0, 32.34), heading = 315.0, scale = 1.0},
+    
+    -- Paleto Bay
+    {prop = 'prop_xmas_tree_int', coords = vector3(-278.26, 6230.73, 31.7), heading = 0.0, scale = 2.0},
+    {prop = 'prop_boxpile_07d', coords = vector3(-276.0, 6230.0, 31.7), heading = 0.0, scale = 1.0},
+    
+    -- Beach - Vespucci
+    {prop = 'prop_xmas_tree_int', coords = vector3(-1291.0, -1266.0, 4.0), heading = 0.0, scale = 2.0},
+    {prop = 'prop_xmas_ext', coords = vector3(-1288.0, -1266.0, 4.0), heading = 90.0, scale = 1.0},
+    {prop = 'prop_xmas_ext', coords = vector3(-1294.0, -1266.0, 4.0), heading = 270.0, scale = 1.0},
+    
+    -- Del Perro Pier
+    {prop = 'prop_xmas_tree_int', coords = vector3(-1644.0, -1019.0, 13.0), heading = 0.0, scale = 2.5},
+    {prop = 'prop_boxpile_07d', coords = vector3(-1642.0, -1016.0, 13.0), heading = 45.0, scale = 1.0},
+    {prop = 'prop_boxpile_07d', coords = vector3(-1646.0, -1022.0, 13.0), heading = 135.0, scale = 1.0},
+    
+    -- Casino
+    {prop = 'prop_xmas_tree_int', coords = vector3(925.0, 46.0, 81.0), heading = 0.0, scale = 2.0},
+    {prop = 'prop_xmas_ext', coords = vector3(922.0, 46.0, 81.0), heading = 0.0, scale = 1.0},
+    {prop = 'prop_xmas_ext', coords = vector3(928.0, 46.0, 81.0), heading = 180.0, scale = 1.0},
+    
+    -- Airport
+    {prop = 'prop_xmas_tree_int', coords = vector3(-1042.0, -2746.0, 21.36), heading = 0.0, scale = 2.5},
+    {prop = 'prop_boxpile_07d', coords = vector3(-1040.0, -2743.0, 21.36), heading = 0.0, scale = 1.0},
+    
+    -- Grove Street
+    {prop = 'prop_xmas_tree_int', coords = vector3(-48.0, -1756.0, 29.42), heading = 0.0, scale = 1.5},
+    {prop = 'prop_xmas_ext', coords = vector3(-46.0, -1756.0, 29.42), heading = 90.0, scale = 1.0},
+    
+    -- Maze Bank Arena
+    {prop = 'prop_xmas_tree_int', coords = vector3(-257.0, -2024.0, 30.0), heading = 0.0, scale = 3.0},
+    {prop = 'prop_boxpile_07d', coords = vector3(-255.0, -2021.0, 30.0), heading = 45.0, scale = 1.5},
+    {prop = 'prop_boxpile_07d', coords = vector3(-259.0, -2027.0, 30.0), heading = 225.0, scale = 1.5},
+    
+    -- Gas Stations - Various Locations
+    {prop = 'prop_xmas_tree_int', coords = vector3(49.42, 2778.79, 58.04), heading = 0.0, scale = 1.5},
+    {prop = 'prop_xmas_tree_int', coords = vector3(1181.98, -330.37, 69.31), heading = 0.0, scale = 1.5},
+    {prop = 'prop_xmas_tree_int', coords = vector3(620.84, 269.1, 103.09), heading = 0.0, scale = 1.5},
+    {prop = 'prop_xmas_tree_int', coords = vector3(2679.94, 3263.95, 55.24), heading = 0.0, scale = 1.5},
+    {prop = 'prop_xmas_tree_int', coords = vector3(2558.83, 380.74, 108.62), heading = 0.0, scale = 1.5},
+    
+    -- 24/7 Stores
+    {prop = 'prop_xmas_ext', coords = vector3(373.0, 327.0, 103.56), heading = 0.0, scale = 1.0},
+    {prop = 'prop_xmas_ext', coords = vector3(-3243.0, 1000.0, 12.83), heading = 0.0, scale = 1.0},
+    {prop = 'prop_xmas_ext', coords = vector3(1728.0, 6414.0, 35.03), heading = 0.0, scale = 1.0},
+    {prop = 'prop_xmas_ext', coords = vector3(1697.0, 4924.0, 42.06), heading = 0.0, scale = 1.0},
+    {prop = 'prop_xmas_ext', coords = vector3(-1820.0, 792.0, 138.08), heading = 0.0, scale = 1.0},
+    {prop = 'prop_xmas_ext', coords = vector3(1163.0, -323.0, 69.2), heading = 0.0, scale = 1.0},
+    {prop = 'prop_xmas_ext', coords = vector3(-707.0, -914.0, 19.21), heading = 0.0, scale = 1.0},
+    {prop = 'prop_xmas_ext', coords = vector3(-47.0, -1757.0, 29.42), heading = 0.0, scale = 1.0},
+    {prop = 'prop_xmas_ext', coords = vector3(25.0, -1346.0, 29.49), heading = 0.0, scale = 1.0},
+    {prop = 'prop_xmas_ext', coords = vector3(1134.0, -982.0, 46.41), heading = 0.0, scale = 1.0},
+    
+    -- Random Street Decorations
+    {prop = 'prop_boxpile_07d', coords = vector3(215.0, -810.0, 30.73), heading = 0.0, scale = 1.0},
+    {prop = 'prop_boxpile_07d', coords = vector3(-1486.0, -378.0, 40.16), heading = 0.0, scale = 1.0},
+    {prop = 'prop_boxpile_07d', coords = vector3(-1305.0, -834.0, 17.15), heading = 0.0, scale = 1.0},
+    {prop = 'prop_boxpile_07d', coords = vector3(89.0, -1982.0, 20.85), heading = 0.0, scale = 1.0},
+    {prop = 'prop_boxpile_07d', coords = vector3(405.0, -1526.0, 29.29), heading = 0.0, scale = 1.0},
+}
