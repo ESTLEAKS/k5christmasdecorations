fc8d3b0135959ecb876c69c71e265943419315b0
ESTLEAKS <leaksmidnightmafia@gmail.com>
2025-10-25 02:00:52 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'k5'
+description 'Christmas Props Around The Map'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
